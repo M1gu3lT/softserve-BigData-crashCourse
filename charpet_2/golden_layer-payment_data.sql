75fbcdda8360e04adf40042e17712646a6ba824a
MiguelTopete <miguel14032020@gmail.com>
2026-05-28 17:44:51 -0600

    se agregan scripts

---
+++ charpet_2/golden_layer-payment_data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE
+golden_layer.payment_data AS
+SELECT
+fnu.unique_id,
+df.clave,
+fnu.download_speed,
+fnu.upload_speed,
+fnu.session_duration,
+fnu.consumer_traffic,
+((fnu.download_speed + fnu.upload_speed + 1)/2) +
+(fnu.consumer_traffic / (fnu.session_duration + 1)) AS payment_amount
+FROM
+silver_layer.fact_network_usage fnu
+JOIN
+silver_layer.dim_finance df
+ON
+fnu.unique_id = df.unique_id
+
+
+SELECT
+*
+FROM GOLDEN_LAYER.payment_data
+LIMIT 10
